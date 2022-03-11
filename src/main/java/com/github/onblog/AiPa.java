500feda19091106ae71b589bd4ad944ea93ac79f
Martin <github521@outlook.com>
2022-03-11 17:52:28 +0800

    commit message

---
+++ src/main/java/com/github/onblog/AiPa.java
@@ -0,0 +1,15 @@
+package com.github.onblog;
+
+import com.github.onblog.executor.AiPaExecutor;
+import com.github.onblog.worker.AiPaWorker;
+
+/**
+ * Create by Martin 2018/9/27/027 14:38
+ */
+public class AiPa {
+
+    public static AiPaExecutor newInstance(AiPaWorker aiPaWorker){
+        return new AiPaExecutor(aiPaWorker);
+    }
+
+}
---
+++ src/main/java/com/github/onblog/worker/AiPaWorker.java
@@ -0,0 +1,26 @@
+package com.github.onblog.worker;
+
+
+import com.github.onblog.util.AiPaUtil;
+import org.jsoup.nodes.Document;
+
+/**
+ * 用户必须实现的接口
+ * Create by Martin 2018/9/26/026 20:19
+ */
+public interface AiPaWorker<T,S> {
+    /**
+     * 如何解析爬下来的HTML文档？
+     * @param doc JSOUP提供的文档
+     * @param util 爬虫工具
+     * @return
+     */
+    T run(Document doc, AiPaUtil util);
+
+    /**
+     * run方法异常则执行fail方法
+     * @param link 网址
+     * @return
+     */
+    S fail(String link);
+}
